2a971289649e510cda4c7b095b10583f69d2eb92
Karl Hudgell <karl.hudgell@bjss.com>
2023-12-08 20:32:35 +0000

    revert octo

--- src/widgets/octoprint/widget.js
+++ src/widgets/octoprint/widget.js
@@ -1,17 +1,17 @@
-// import octoprintProxyHandler from "./proxy";
+import genericProxyHandler from "utils/proxy/handlers/generic";
 
-// const widget = {
-//   api: "{url}/api/{endpoint}?apikey={key}",
-//   proxyHandler: octoprintProxyHandler,
+const widget = {
+  api: "{url}/api/{endpoint}?apikey={key}",
+  proxyHandler: genericProxyHandler,
 
-//   mappings: {
-//     printer_stats: {
-//       endpoint: "printer",
-//     },
-//     job_stats: {
-//       endpoint: "job",
-//     },
-//   },
-// };
+  mappings: {
+    printer_stats: {
+      endpoint: "printer",
+    },
+    job_stats: {
+      endpoint: "job",
+    },
+  },
+};
 
-// export default widget;
+export default widget;
